c48feece85458c30acfbda572aafde75ba080239
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-10-18 22:24:34 +0800

    small issues fixes

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -8,6 +8,7 @@ const QUERY_ALL_USERS = gql`
       name
       username
       nationality
+      age
     }
   }
 `;
@@ -36,6 +37,8 @@ const CREATE_USER_MUTATION = gql`
     createUser(input: $input) {
       name
       id
+      age
+      nationality
     }
   }
 `;
@@ -49,7 +52,7 @@ function DisplayData() {
   const [username, setUsername] = useState('');
   const [nationality, setNationality] = useState('');
 
-  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data, loading, error, refetch } = useQuery(QUERY_ALL_USERS);
   const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
   const [fetchMovie, { data: movieSearchData, error: movieError }] =
     useLazyQuery(GET_MOVIE_BYNAME);
@@ -103,9 +106,16 @@ function DisplayData() {
           onClick={() => {
             createUser({
               variables: {
-                input: { name: name, username, age: 21, nationality },
+                input: {
+                  name: name,
+                  username,
+                  age: Number(age),
+                  nationality: nationality,
+                },
               },
             });
+
+            refetch();
           }}
         >
           Create User
@@ -118,7 +128,7 @@ function DisplayData() {
             <div>
               <h1>Name: {user.name}</h1>
               <h1>Username: {user.username}</h1>
-              <h1>age: {user.age}</h1>
+              <h1>Age: {user.age}</h1>
               <h1>Nationality: {user.nationality}</h1>
             </div>
           );
